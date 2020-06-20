9350b6b00218b2aab1432aad05cdb350e145effc
imdhemy <imdhemy@gmail.com>
2020-06-21 01:46:09 +0200

    reformat
    - install laravel-mix
    - Create react application

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import App from './App'
+import ReactDOM from 'react-dom'
+
+function createAppRoot () {
+  let relatedVideosContainer = document.getElementById('related')
+  let appContainer = document.createElement('div')
+  relatedVideosContainer.prepend(appContainer)
+  return appContainer
+}
+
+let root = createAppRoot()
+ReactDOM.render(<App/>, root)
---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+let mix = require('laravel-mix')
+
+mix.js('src/background.js', 'dist').
+  react('src/index.js', 'dist').
+  copy('src/assets', 'dist/assets').
+  copy('src/manifest.json', 'dist').
+  copy('src/options.html', 'dist').
+  copy('src/popup.html', 'dist').
+  setPublicPath('dist')
