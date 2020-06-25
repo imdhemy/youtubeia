e5a2900e7bbd967ccec9d48f795cf559aae9ecf9
imdhemy <imdhemy@gmail.com>
2020-06-25 12:39:41 +0200

    Set up base layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
-import { Text, View } from 'react-native-web'
+import { Container, Tab } from 'semantic-ui-react'
+
+const panes = [
+  {
+    menuItem: { key: 'users', icon: 'users', content: 'Users' },
+    render: () => <Tab.Pane attached='top' style={{ height: '600px' }}>Tab 1 Content</Tab.Pane>,
+  },
+  {
+    menuItem: 'Tab 2',
+    render: () => <Tab.Pane attached='top' style={{ height: '600px' }}>Tab 2 Content</Tab.Pane>,
+  },
+  {
+    menuItem: 'Tab 3',
+    render: () => <Tab.Pane attached='top' style={{ height: '600px' }}>Tab 3 Content</Tab.Pane>,
+  },
+]
 
 class App extends Component {
   render () {
     return (
-      <Container>
-        <View>
-          <Text>React native web</Text>
-        </View>
+      <Container style={{ minHeight: '640px' }}>
+        <Tab menu={{ attached: 'bottom', tabular: false }}
+             panes={panes}/>
       </Container>
     )
   }
---
+++ src/screens/AboutScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AboutScreen extends Component {
+  render () {
+    return (
+      <div>
+        <h1>About Screen</h1>
+      </div>
+    )
+  }
+}
+
+export default AboutScreen
